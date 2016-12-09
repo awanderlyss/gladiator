cc04c03eb7aa13bd9f05c5612b4b39a65d1545ae
awanderlyss <alyssa.felix7@gmail.com>
2016-12-09 14:11:34 -0500

    Create  method, pass test #gladiators & #add_gladiator

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,8 +1,17 @@
 class Arena
   attr_accessor :name, :gladiators
 
+  # name has a name, is always capitalized
   def initialize(name)
     @name = name.capitalize
+    # gladiators starts empty
     @gladiators = []
+  end
+
+  # add_gladiator adds a gladiator to the arena, can add multiple gladiators, does not have more than two gladiators at once
+  def add_gladiator(gladiator)
+    if @gladiators.length < 2
+      @gladiators << gladiator
+    end
   end
 end
